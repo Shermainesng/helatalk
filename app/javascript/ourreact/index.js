5c2f7af0b08c8576c3753a7ccaa897bac594e6fe
jiaen zhang <jiaen.zhang98@gmail.com>
2022-02-10 17:55:55 +0800

    setup react components

--- app/javascript/ourreact/index.js
+++ app/javascript/ourreact/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
 
 // Components
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Blogs from "./components/Blogs";
 import Events from "./components/Events";
 import Discover from "./components/Discover";
@@ -16,24 +16,28 @@ const reducers = combineReducers({});
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = composeEnhancers(applyMiddleware(reduxPromise));
 
+// function Main() {
+//   return (
+//     <Provider store={createStore(reducers, {}, middlewares)}>
+//       <BrowserRouter>
+//         <FlashMessages messages={state.flashMessages} />
+//         <Header />
+//         {/* pass the state as props to header to ensure header can still render properly */}
+//         <Routes>
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/blogs" element={<Blogs />} />
+//           <Route path="/events" element={<Events />} />
+//           <Route path="/discover" element={<Discover />} />
+//         </Routes>
+//         <Footer />
+//       </BrowserRouter>
+//     </Provider>
+//   );
+// }
+
 function Main() {
-  return (
-    <Provider store={createStore(reducers, {}, middlewares)}>
-      <BrowserRouter>
-        <FlashMessages messages={state.flashMessages} />
-        <Header />
-        {/* pass the state as props to header to ensure header can still render properly */}
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/discover" element={<Discover />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </Provider>
-  );
+  return(<h1>Hi</h1>);
 }
 
 ReactDOM.render(<Main />, document.querySelector("#app"));
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/about', to: 'pages#about'
-  get '/blogs', to: 'pages#blogs'
-  get '/explore', to: 'pages#explore'
+
+  get '/about', to: 'pages#home'
+  get '/blog', to: 'pages#home'
+  get '/discover', to: 'pages#home'
+  get '/events', to: 'pages#home'
 end
