fa5b36490294772e13d78f51d3194229c4b9df40
jiaen zhang <jiaen.zhang98@gmail.com>
2022-02-14 14:36:08 +0800

    created footer

--- app/javascript/ourreact/components/Footer.js
+++ app/javascript/ourreact/components/Footer.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Container from "./Container";
 
 function Footer() {
   return (
-    <di className="footer">
-      Footer
+    <div className="bg-dark py-4">
+      <Container>
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3">
+          <div className="col-md-4 d-flex align-items-center">
+            <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+              icon
+            </a>
+            <span className="text-muted">© 2021 Company, Inc</span>
+          </div>
+
+          <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li className="ms-3"><a className="text-muted" href="#">icon</a></li>
+            <li className="ms-3"><a className="text-muted" href="#">icon</a></li>
+            <li className="ms-3"><a className="text-muted" href="#">icon</a></li>
+          </ul>
+        </footer>
+      </Container>
     </div>
   );
 }
