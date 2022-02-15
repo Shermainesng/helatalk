f7ed11f71d83756fd6ab9fd0a04a5deb8689f90e
Shermainesng <shersmallsng@gmail.com>
2022-02-15 22:04:50 +0800

    fixed navbar and stuff

--- app/javascript/ourreact/components/About.js
+++ app/javascript/ourreact/components/About.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Page from "./Page";
+import Container from "./Container";
 
 function About() {
   const title = "About Us";
   return (
-    <Page id="about" title={title} classes="text-center">
-      <h2 className="font-white font-headers">our story</h2>
-      <p className="font-white font-body w-75 m-auto">hey there! you may have found us on tiktok or instagram, giving out sex tips. but we are more than that. we believe taking a holistic approach to achieve enthusiastic, consent-driven sex, with sexual health and wellness at our core messaging. our goal is to build a community of like-minded, sex-positive people where we can have events, discussions and just being there for each there.</p>
-      <h3>if you believe in this vision, join our community and spread the word</h3>
+    <Page id="about" title={title}>
+      <Container classes="text-center ">
+        <div>
+          <h2 className="font-white font-headers">nice to meet you</h2>
+        </div>
+        <div className="about-more">
+          <p className="font-white font-body w-75 m-auto">we're HELA! you may have found us on tiktok or instagram, giving out sex tips. but we are more than that. we believe taking a holistic approach to achieve enthusiastic, consent-driven sex, with sexual health and wellness at our core messaging. our goal is to build a community of like-minded, sex-positive people where we can have events, discussions and just being there for each there.</p>
+          <h3>if you believe in this vision, join our community and spread the word</h3>
+        </div>
+      </Container>
     </Page>
   );
 }
