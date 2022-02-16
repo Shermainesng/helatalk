fb3b4afa20ffdfa6ee43acd5f28e2a598f97fe67
jiaen zhang <jiaen.zhang98@gmail.com>
2022-02-16 15:21:13 +0800

    styled blog pages

--- app/javascript/ourreact/components/BlogCategory.js
+++ app/javascript/ourreact/components/BlogCategory.js
@@ -1,61 +1,50 @@
 import React, { useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 import axios from 'axios';
 
 import Page from "./Page";
 import ArticleCard from "./ArticleCard";
 
-
-
-const articles = [
-  { title: "How to ride your man", id: 1, publisher: "Bad girls bible", author: "autho"},
-  { title: "vaginal orgasm", id: 2, publisher: "Bad girls bible", author: "authorname"},
-  { title: "cat person and me", id: 3, publisher: "slate", author: "authorname"},
-  { title: "sexual desire", id: 4, publisher: "the cut", author: "authorname"}
-]
-
 function BlogCategory(props) {
   const params = useParams();
   const [category, setCategory] = useState(params.category.toLowerCase());
   const [articles, setArticles] = useState([]);
+  const categories = ["sexuality", "mental health", "sexual health", "self-pleasure"];
 
   useEffect(() => {
-    console.log(`/api/articles/${category.toLowerCase()}`);
+    // console.log(`/api/articles/${category.toLowerCase()}`);
     // Make a request for a user with a given ID
     axios.get(`/api/articles/${category}`)
       .then(function (response) {
-        console.log(response.data);
+        // console.log(response.data);
         setArticles(response.data);
       })
       .catch(function (error) {
         // handle error
         console.log(error);
       });
-  });
+  }, []);
 
-  return (
-    <Page id={category} title={category} classes="p-3 my-4 mx-5">
-      <h1 className="font-headers font-white">blog</h1>
-      <h2 className="font-body font-white">articles about <em>{category}</em></h2>
-      <ul className="nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#" onClick={() => setCategory("kink")}>kink</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#" onClick={() => setCategory("health")}>health</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-
-      <div className="w-75">
-        {articles.map((article) => <ArticleCard article={article} key={article.id} />) }
+  return(
+    <Page id={category} title={category} classes="p-3 my-4 mx-5 vh-100">
+      <h1 className="font-anton font-white text-center">blog</h1>
+      <h2 className="font-poppins font-body font-white text-center">articles and resources we love</h2>
+      <div className="wh-100 linear-gradient p-4">
+        <ul className="nav font-white font-poppins">
+          {categories.map((category, index) => {
+            return(
+            <li className="nav-item">
+              <NavLink className={({ isActive }) => isActive ? "hela-nav-link active" : "hela-nav-link"} to={`/blog/${category.replace(/\W/, '')}`} key={index}>
+                {category}
+              </NavLink>
+            </li>
+            );})}
+        </ul>
+        <div className="p-2">
+          {articles.map((article) => <ArticleCard article={article} key={article.id} />) }
+        </div>
       </div>
-    </Page>
+  </Page>
   );
 }
 
