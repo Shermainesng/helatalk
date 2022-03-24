12a25b8330e3b3437ad8132f5124795f839c9b37
jiaen zhang <jiaen.zhang98@gmail.com>
2022-03-24 12:37:44 +0800

    added statue image and poppers for discover

--- app/javascript/ourreact/components/Discover.js
+++ app/javascript/ourreact/components/Discover.js
@@ -1,6 +1,8 @@
-import React from "react";
-import Page from "./Page";
-import Container from "./Container";
+import React from "react"
+import Page from "./Page"
+import Container from "./Container"
+import Tooltip from "@mui/material/Tooltip"
+import TransitionsPopper from "./TransitionsPopper"
 
 function Discover() {
   const title = "Discover"
@@ -10,9 +12,11 @@ function Discover() {
       <Container classes="text-center">
         <h1 className="font-headers font-white mb-1">watch this page!</h1>
         <h2 className="font-white font-body mb-4">we've got something cool for you</h2>
+
+        <TransitionsPopper content="breast" />
       </Container>
     </Page>
-  );
+  )
 }
 
-export default Discover;
+export default Discover
